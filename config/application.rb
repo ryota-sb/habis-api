8ec5d1baa6ba02d6f2962e485ff18781d9f165cd
ryota-sb <ryota.sb.1206@gmail.com>
2020-10-03 23:56:51 +0900

    init

---
+++ config/application.rb
@@ -0,0 +1,26 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module HabisApi
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.api_only = true
+    config.generator do |g|
+      g.test_framework false
+    end
+  end
+end
