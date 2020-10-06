a511c86e9d60a676b15bcf56d672344ed4bd95f3
ryota-sb <ryota.sb.1206@gmail.com>
2020-10-06 21:48:29 +0900

    task model validates

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
+  validates :content, presence: true, length: { in: 1..40 }
+  validates :week, presence: true
+  validates :notification_time, presence: true
   belongs_to :user
 end
