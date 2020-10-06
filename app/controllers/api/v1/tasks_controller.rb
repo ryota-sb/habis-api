17df1cd0976aca9289a126af34c539d17ee63968
ryota-sb <ryota.sb.1206@gmail.com>
2020-10-06 19:06:25 +0900

    tasks controller 追加

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  def index
+    tasks = Task.all.order(created_at: :desc)
+    length = tasks.length
+    render json: { status: 'success', message: 'loaded tasks', data: tasks, length: length }
+  end
+
+  def show
+    render json: { status: 'success', message: 'loaded the task', data: @task }
+  end
+
+  def create
+    task = Task.new(task_params)
+    if task.save
+      render json: { status: 'success', data: task }
+    else
+      render json: { status: 'error', data: task.errors }
+    end    
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: { status: 'success', message: 'update the task', data: @task }
+    else
+      render json: { status: 'error', message: 'not updated', data: @task.errors }
+    end
+  end
+
+  def destroy
+    @task.destroy
+    render json: { status: 'success', message: 'deleted the task', data: @task }
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :is_done, :week, :notification_time, :user_id)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     mount_devise_token_auth_for 'User', at: 'auth'
+    namespace :v1 do
+      resources :tasks, only: [:index, :show, :create, :update, :destroy]
+    end
   end
 end
