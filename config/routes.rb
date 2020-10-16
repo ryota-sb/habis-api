1361eb11a1cc4b952755e86d88ae83902a051b23
ryota-sb <ryota.sb.1206@gmail.com>
2020-10-16 21:52:24 +0900

    route 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    mount_devise_token_auth_for 'User', at: 'auth'
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'api/auth/registrations'
+    }
     namespace :v1 do
       resources :tasks, only: [:index, :show, :create, :update, :destroy]
     end
