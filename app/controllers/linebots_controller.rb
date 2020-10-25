c6138030be9f48e1708750210fdb650f1de7f16f
ryota-sb <ryota.sb.1206@gmail.com>
2020-10-25 13:12:19 +0900

    line client 構文修正

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -57,7 +57,7 @@ class LinebotsController < ApplicationController
 
   # Lineアカウント取得
   def client
-    @client ||= Line::Bot::Client.new |config| do
+    @client ||= Line::Bot::Client.new do |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     end
